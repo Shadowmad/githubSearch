09bd0d002019214b3481fe7afdd719e0606e874d
Shadowmad <maksud.aghayev@gmail.com>
2016-02-21 14:27:09 -0800

    commit 4

--- controllers/multipleController.php
+++ controllers/multipleController.php
@@ -3,6 +3,10 @@
   require_once($_SERVER['DOCUMENT_ROOT'] . "core/filters.php");
   $connector = new Database();
   $filter = new Filters();
+
+
+  $_POST = json_decode(file_get_contents('php://input'), true);
+  print_r($_POST);
   $request = array("projectID","projectName","projectStars","language");
   $data = json_encode(array(  "records" =>  $filter->openPDOObject($connector->databaseRead($request, "reposTable") ) ) );
   echo ($data);
--- presentation/js/appControllers.js
+++ presentation/js/appControllers.js
@@ -10,44 +10,94 @@ scotchApp.controller('singleOutputController', function($scope,$http,$routeParam
     });
 });
 
-scotchApp.controller('multipleOutputController', function($scope,$http) {
-    $http.get("/controllers/multipleController.php")
+scotchApp.controller('multipleOutputController', function($scope,getFullListByFilter) {
+    /*$http.get("/controllers/multipleController.php?date")
     .then(function(responce){
       $scope.messages = responce.data.records;
+    });*/
+    $scope.messages = {};
+    var scope = $scope;
+    getFullListByFilter.get().then(function(data) {
+      scope.messages = data.data.records;
     });
+
 });
 
+scotchApp.factory('getFilters', ['$http', function($http){
+  var getFiltersFactory = {};
+  getFiltersFactory.data = '';
 
-scotchApp.controller("filterController", function($scope,$location,$http){
-  $scope.slider = {
-    min: 0,
-    max: 50000,
+  getFiltersFactory.set = function(sendingVar){
+    this.data = sendingVar;
+    return this.data;
+  }
+  getFiltersFactory.get = function(){
+    return this.data;
+  }
+  return getFiltersFactory;
+
+}]);
+
+scotchApp.factory('getFullListByFilter', ['$http','getFilters', function($http, getFilters) {
+  var getFullListByFilterFactory = {};
+
+  getFullListByFilterFactory.data = '';
+  getFullListByFilterFactory.filter = '';
+
+  getFullListByFilterFactory.getFilter = function(){
+    this.filter = getFilters.get();
+    return this.filter;
+  }
+  getFullListByFilterFactory.load = function() {
+    this.getFilter();
+    /*this.data = $http.get('/controllers/multipleController.php').then(function(data) {
+      return data;
+    });*/
+    this.data = $http({
+      method: "post",
+      url: "/controllers/multipleController.php",
+      data: this.filter,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).then(function(data){
+      return data;
+    });
+    return this.data;
+  };
+
+  getFullListByFilterFactory.get = function() {
+    return this.data === '' ? this.load() : this.data;
+  };
+
+  return getFullListByFilterFactory;
+}]);
+
+
+scotchApp.controller("filterController", function($scope,$http,$location, getFilters){
+  $scope.itemsDateSelect =  [
+                              {name: 'allDates', value:'Select all dates'},
+                              {name: 'beforeDates', value:'Select dates before'},
+                              {name: 'afterDates', value:'Select dates after'},
+                              {name: 'rangeDates', value:'Select range of dates'},
+                            ]
+
+
+  $scope.filterData = {};
+  $scope.filterData.slider = {
+    minValue: 0,
+    maxValue: 50000,
     options: {
       floor: 0,
       ceil: 50000
     }
   };
-  $scope.processForm = function() {
-    $scope.formData = {};
-    $http({
-    method  : 'POST',
-    url     : '/controllers/multipleController.php',
-    data    : $.param($scope.formData),  // pass in data as strings
-    headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-   })
-    .success(function(data) {
-      console.log(data);
 
-      if (!data.success) {
-        // if not successful, bind errors to error variables
-        $scope.errorName = data.errors.name;
-        $scope.errorSuperhero = data.errors.superheroAlias;
-      } else {
-        // if successful, bind success message to message
-        $scope.message = data.message;
-      }
-    });
-  };
+  var scope = $scope;
+  $scope.processForm = function() {
+      getFilters.set($scope.filterData);
+      $location.path("/repos/");
+  }
+
+
   $scope.getLangs = function(){
     $http.get("/controllers/populateController.php")
     .then(function(responce){
